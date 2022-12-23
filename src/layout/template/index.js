e089f10cce635e43eb90e59949f641ebbcff0596
summernian <35884693+summernian@users.noreply.github.com>
2022-12-23 18:04:56 +0800

    Delete src directory

--- src/layout/template/index.js
+++
@@ -1,1 +0,0 @@
-export { default as sideBar } from './sideBar/index.vue';
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-const getters = {
-	token: state => state.user.token,
-	roles: state => state.user.userInfo.role || '',
-	baseRoute: state => state.permission.routes
-};
-export default getters;
