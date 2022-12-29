0c86b87110aa84ce5ef2519191fd004553cd81bf
summernian <35884693+summernian@users.noreply.github.com>
2022-12-29 20:45:19 +0800

    Delete src directory

--- src/plugin/svgIcons/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import SvgIcon from './SvgIcon';
-Vue.component('c-svg-icon', SvgIcon);
-
-// 加载svg资源
-const requireSvg = reqireContext => reqireContext.keys().map(reqireContext);
-const req = require.context('./svg', false, /\.svg$/);
-requireSvg(req);
